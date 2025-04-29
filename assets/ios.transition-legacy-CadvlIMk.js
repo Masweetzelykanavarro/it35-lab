095ff82d490da438a788593dab9aa2758345eabf
Masweetzelykanavarro <masweetzelykanavarro@gmail.com>
2025-04-29 18:07:47 +0800

    Updates

--- assets/ios.transition-legacy-CadvlIMk.js
+++ assets/ios.transition-legacy-CadvlIMk.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CPUFEPqr.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CSPFJUE-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
